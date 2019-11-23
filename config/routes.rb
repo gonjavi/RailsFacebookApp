d98eb865194923d75e0ed206ae4f8285d3a8fe87
gonjavi <javiercaliescali@gmail.com>
2019-11-22 19:59:08 -0500

    creating devise user and post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'homes/index'
+  root to: "homes#index"
+  devise_for :users
 end
---
+++ db/migrate/20191122230035_add_name_picture_to_user.rb
@@ -0,0 +1,6 @@
+class AddNamePictureToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :picture, :string
+  end
+end
