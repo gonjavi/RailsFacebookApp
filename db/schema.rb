3e8d62192d2b609d5e0345d601b1336b76c1beb1
gonjavi <javiercaliescali@gmail.com>
2019-11-29 12:42:24 -0500

    checking errors

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_234245) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '' null: false
+    t.string 'email', null: false
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
     t.datetime 'reset_password_sent_at'
