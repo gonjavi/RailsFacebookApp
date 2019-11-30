f6291d631e5ff62e224feca4a5617b36b1be4ab6
gonjavi <javiercaliescali@gmail.com>
2019-11-30 16:03:04 -0500

    adding has many posts association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts,dependent: :destroy
   validates :name, presence: { string: true }, length: { minimum: 2 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
   devise_for :users
-  resources :users, only: [:show]
 end
