c934686d3f16d50ed0b2d3bbb3b542f38bbbefb4
gonjavi <javiercaliescali@gmail.com>
2020-06-06 23:10:12 -0500

    add picture to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,9 @@ class PostsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:content,
-                                 :comments_attributes, %i[id content user_id])
+                                 :image,
+                                 :comments_attributes,
+                                  %i[id content image user_id])
   end
 
   def comment_params
